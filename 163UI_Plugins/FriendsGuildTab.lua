fe4d26d0bca4f699220a352e9ebfb0b760b93ab4
aby-ui <aby_ui@163.com>
2019-12-26 01:52:34 +0800

    auto 20191226

--- 163UI_Plugins/FriendsGuildTab.lua
+++ 163UI_Plugins/FriendsGuildTab.lua
@@ -2,7 +2,7 @@ U1PLUG["FriendsGuildTab"] = function()
 
 local tabs = FriendsFrame.numTabs
 local name = "FriendsFrameTab_OpenGuild"
-WW:Button(name, FriendsFrame, "FriendsFrameTabTemplate"):SetText("会"):LEFT("$parentTab"..tabs, "RIGHT", -14, 0)
+WW:Button(name, FriendsFrame, "FriendsFrameTabTemplate"):SetText(GUILD):LEFT("$parentTab"..tabs, "RIGHT", -14, 0)
 :SetScript("OnClick", function()
     if not GuildFrame or not GuildFrame:IsVisible() then
         ToggleFriendsFrame()
@@ -11,8 +11,15 @@ WW:Button(name, FriendsFrame, "FriendsFrameTabTemplate"):SetText("会"):LEFT("$p
 end):un()
 _G[name.."Left"]:SetVertexColor(0,1,0) _G[name.."Middle"]:SetVertexColor(0,1,0) _G[name.."Right"]:SetVertexColor(0,1,0)
 PanelTemplates_DeselectTab(FriendsFrameTab_OpenGuild)
+PanelTemplates_TabResize(FriendsFrameTab_OpenGuild, 0)
 --PanelTemplates_SetNumTabs(FriendsFrame, tabs+1) --污染
 --PanelTemplates_UpdateTabs(FriendsFrame)
+
+local QUICK_JOIN_SHORT = GetLocale():sub(1,2)=="zh" and "加入" or QUICK_JOIN
+hooksecurefunc("FriendsFrame_UpdateQuickJoinTab", function(numGroups)
+    FriendsFrameTab4:SetText(QUICK_JOIN_SHORT .. " "..string.format(NUMBER_IN_PARENTHESES, numGroups));
+    PanelTemplates_TabResize(FriendsFrameTab4, 0);
+end)
 
 CoreDependCall("Blizzard_GuildUI", function()
     GuildFrameTab1:SetPoint("BOTTOMLEFT", -7, -30)
