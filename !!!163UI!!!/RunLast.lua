8b37ec0c08381f2dab07ecfc334cad2ee74d5e9e
aby-ui <aby_ui@163.com>
2021-02-11 18:53:31 +0800

    auto 20210211

--- !!!163UI!!!/RunLast.lua
+++ !!!163UI!!!/RunLast.lua
@@ -23,16 +23,16 @@ CoreRegisterEvent("INIT_COMPLETED", {
     INIT_COMPLETED = function()
         if not U1DBG then return end
         local now = date("%Y%m%d")
-        if now >= "20200124" and now <= "20200130" then
+        if now >= "20210211" and now <= "20210217" then
             local greetings = {
-                {"大年三十啦，愿大家免疫病痛，平安快乐"}, {"初一","数你给力"}, {"初二", "新春快乐"}, {"初三","手红不用肝"}, {"初四", "突袭顺利"}, {"初五", "低保到手"}, {"初六", "引领潮流"}
+                {"大年三十啦，愿大家身体健康，万事如意"}, {"初一", "首胜顺利"}, {"初二", "大帝一把过"}, {"初三", "骑上粉火箭"}, {"初四", "进组顺利"}, {"初五", "开出幽灵虎"}, {"初六", "低保到手"}
             }
             if U1DBG.GreetingCNY ~= now then
                 U1DBG.GreetingCNY = now
-                local day = tonumber(now) - 20200124 + 1
+                local day = tonumber(now) - 20210211 + 1
                 if greetings[day] then
-                    C_Timer.After(1, function()
-                        U1Message(#greetings[day] == 1 and greetings[day][1] or "大年"..greetings[day][1].."，爱不易祝您鼠年大吉，"..greetings[day][2].."！", 1, 1, 0)
+                    C_Timer.After(3, function()
+                        U1Message(#greetings[day] == 1 and greetings[day][1] or "大年"..greetings[day][1].."，爱不易祝您牛年大吉，"..greetings[day][2].."！", 1, 1, 0)
                     end)
                 end
             end
